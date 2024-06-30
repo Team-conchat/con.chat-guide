7930dd1e518403026c63f6454d0688cf4428ef39
eunmmm <rosiebaek223@gmail.com>
2024-06-30 16:17:12 +0900

    Setting: [T-28] 리액트 초기 세팅 및 패키지 설정
    - ESLint, Prettier, Husky, lint-staged 등 개발 도구 추가
    
    https://www.notion.so/T-28-Guide-page-734de37c67b14787aa56efe4ef7ea077?pvs=4

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react', 'jsx-a11y', 'import', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
+};
