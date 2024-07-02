4ab59c1ae12b1e9fbaf637407c5418b3d71f3444
eunmmm <rosiebaek223@gmail.com>
2024-07-02 15:41:01 +0900

    Feat: [T-29] 페이지 이동 시 화면 최상단으로 스크롤
    https://www.notion.so/T-29-Guide-page-35518035147a46658691e72cb9a56c6f?pvs=4

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+import ScrollToTop from './components/ScrollToTop';
+
 import App from './App';
 
 import './index.css';
@@ -9,6 +11,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
